fddbf9d3f60aa836796a0cc853ecd5772ecab075
zhanghong <shouhulife@gmail.com>
2016-01-14 20:20:18 +0800

    添加用户模型

---
+++ app/src/main/java/recording/com/recording/Model/User.java
@@ -0,0 +1,63 @@
+package recording.com.recording.Model;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by hong on 2016/1/14.
+ */
+public class User extends BmobObject {
+    private String name;
+    private String pwd;
+    private int id;
+    private long date;
+    private String phone;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
